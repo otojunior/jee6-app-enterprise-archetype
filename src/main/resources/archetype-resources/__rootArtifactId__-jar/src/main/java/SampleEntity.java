73b8698fe12d6b0b3eca8826ae3a549dc20b9f75
Oto Junior <otojunior@gmail.com>
2014-01-28 14:13:06 -0200

    Exemplo de hashcode() e equals() gerado pelo Eclipse e toString gerado pelo Commons Lang.

--- src/main/resources/archetype-resources/__rootArtifactId__-jar/src/main/java/SampleEntity.java
+++ src/main/resources/archetype-resources/__rootArtifactId__-jar/src/main/java/SampleEntity.java
@@ -6,6 +6,9 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Version;
 
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+
 /**
  * @author 
  */
@@ -20,4 +23,40 @@ public class SampleEntity {
 	
 	@Column(length=20)
 	private String sampleProperty;
+	
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) return true;
+		if (obj == null) return false;
+		if (getClass() != obj.getClass()) return false;
+		SampleEntity other = (SampleEntity) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public String toString() {
+		return ToStringBuilder.reflectionToString(this, ToStringStyle.SHORT_PREFIX_STYLE);
+	}
 }
