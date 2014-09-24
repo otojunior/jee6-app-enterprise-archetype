0f010048531b0fcc2a3208991ad72fc216b162fc
Oto Junior <otojunior@gmail.com>
2014-09-24 17:16:47 -0300

    Ajustes para adequação ao Sonar

--- src/main/resources/archetype-resources/__rootArtifactId__-jar/src/main/java/SampleEntity.java
+++ src/main/resources/archetype-resources/__rootArtifactId__-jar/src/main/java/SampleEntity.java
@@ -10,19 +10,73 @@ import org.apache.commons.lang3.builder.ToStringBuilder;
 import org.apache.commons.lang3.builder.ToStringStyle;
 
 /**
- * @author 
+ * A Sample Entity.
+ * @author <Author name>
  */
 @Entity
 public class SampleEntity {
+	private static final int COL_LENGTH_SAMPLEPROPERTY = 20;
+	
+	/**
+	 * The entity ID (PK).
+	 */
 	@Id
 	@GeneratedValue
 	private Long id;
 	
+	/**
+	 * The Entity Version (Instance/Tuple Version).
+	 */
 	@Version
 	private Long version;
 	
-	@Column(length=20)
+	/**
+	 * A Sample property.
+	 */
+	@Column(length=COL_LENGTH_SAMPLEPROPERTY)
 	private String sampleProperty;
+	
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		boolean isEquals = true;
+		
+		if (this == obj) {
+			isEquals = true;
+		} else if (obj == null) {
+			isEquals = false;
+		} else if (getClass() != obj.getClass()) {
+			isEquals = false;
+		} else {
+			SampleEntity other = (SampleEntity) obj;
+			if (id == null) {
+				if (other.id != null) {
+					isEquals = false;
+				}
+			} else if (!id.equals(other.id)) {
+				isEquals = false;
+			}
+		}
+		return isEquals;
+	}
+
+	/**
+	 * Gets sampleProperty value 
+	 * @return the property value.
+	 */
+	public String getSampleProperty() {
+		return sampleProperty;
+	}
+
+	/**
+	 * Gets the version value. 
+	 * @return version value.
+	 */
+	public Long getVersion() {
+		return version;
+	}
 	
 	/**
 	 * {@inheritDoc}
@@ -34,22 +88,13 @@ public class SampleEntity {
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		return result;
 	}
-
+	
 	/**
-	 * {@inheritDoc}
+	 * Sets the sample Property.
+	 * @param sampleProperty a sampleProperty value.
 	 */
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj) return true;
-		if (obj == null) return false;
-		if (getClass() != obj.getClass()) return false;
-		SampleEntity other = (SampleEntity) obj;
-		if (id == null) {
-			if (other.id != null)
-				return false;
-		} else if (!id.equals(other.id))
-			return false;
-		return true;
+	public void setSampleProperty(String sampleProperty) {
+		this.sampleProperty = sampleProperty;
 	}
 	
 	/**
