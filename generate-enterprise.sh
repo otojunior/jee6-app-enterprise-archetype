097ed695a9909d5a25bc32d5cef032320a6e5543
Oto Junior <otojunior@gmail.com>
2015-03-12 11:28:28 -0300

    Correção do script de geração de projeto

--- generate-enterprise.sh
+++ generate-enterprise.sh
@@ -8,7 +8,7 @@
 #
 mvn archetype:generate -DarchetypeGroupId=org.otojunior.archetype \
 -DarchetypeArtifactId=jee6-app-enterprise-archetype \
--DarchetypeVersion=1.2-SNAPSHOT \
+-DarchetypeVersion=1.2 \
 -DgroupId=org.otojunior \
 -DartifactId=$1 \
 -Dversion=1.0-SNAPSHOT \
